34f95a46c56ce3959dbcd100ba1a2fa4631fea24
Graham Robinson <graham@yelcho.com.au>
2020-06-15 09:47:26 +1000

    Testing VS code github plugin Fixes #1

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,33 +1,33 @@
-sap.ui.define(["sap/ui/core/mvc/Controller", "sap/base/Log"], function(
+sap.ui.define(["sap/ui/core/mvc/Controller", "sap/base/Log"], function (
 	Controller,
 	Log
 ) {
 	"use strict"
+	// TODO #1 test issue created from VS code
+	// now working on this issue
 	return Controller.extend("yelcho.mydemo.nestcomproute.controller.App", {
-		onInit: function() {
+		onInit: function () {
 			Log.info(this.getView().getControllerName(), "onInit")
 
 			this.getOwnerComponent()
 				.getRouter()
 				.attachRouteMatched(this._onRouteMatched, this)
 		},
-		_onRouteMatched: function(oEvent) {
+		_onRouteMatched: function (oEvent) {
 			Log.info(this.getView().getControllerName(), "_onRouteMatched")
 		},
-		onSideNavButtonPress: function() {
+		onSideNavButtonPress: function () {
 			Log.info(this.getView().getControllerName(), "onSideNavButtonPress")
 			var toolPage = this.byId("toolPage")
 			toolPage.setSideExpanded(!toolPage.getSideExpanded())
 		},
-		onUserNamePress: function() {
+		onUserNamePress: function () {
 			Log.info(this.getView().getControllerName(), "onUserNamePress")
 		},
-		onItemSelect: function(oEvent) {
+		onItemSelect: function (oEvent) {
 			const key = oEvent.getParameter("item").getKey()
 			Log.info(this.getView().getControllerName(), `onItemSelect Key=${key}`)
-			this.getOwnerComponent()
-				.getRouter()
-				.navTo(key)
-		}
+			this.getOwnerComponent().getRouter().navTo(key)
+		},
 	})
 })
